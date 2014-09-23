dbc0e13d8312614e073fb79039f27fcb60c84bda
skad <skadprog@gmail.com>
2014-09-23 22:08:13 +0200

    move code for clearity

--- exemple/index.php
+++ exemple/index.php
@@ -19,8 +19,18 @@ session_start();
 ##include
 #load include the api class
 require('apigenerator.php');
-#check the url element url (see file for details)
-require('checkurl.php');
+
+##exploding url for geting info
+//$url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$url = $_SERVER['REQUEST_URI'];
+$elementsUrl = explode("/",$url);
+##remove 2 frist case because of nulity
+array_splice($elementsUrl,0,2);
+
+for($i=0; $i < count($elementsUrl); $i++)
+{
+	$elementsUrl[$i] = urldecode($elementsUrl[$i]);
+}
 
 #Execute load and execute the api
 $api = new ApiGenerator();
