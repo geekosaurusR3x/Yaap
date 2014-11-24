4c55b2cb215c2d9e9f11bae399403baae7d87ae3
skad <skadprog@gmail.com>
2014-11-24 23:45:43 +0100

    move all into an sub file

--- exemple/index.php
+++ exemple/index.php
@@ -19,18 +19,7 @@ session_start();
 ##include
 #load include the api class
 require('apigenerator.php');
-
-##exploding url for geting info
-//$url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-$url = $_SERVER['REQUEST_URI'];
-$elementsUrl = explode("/",$url);
-##remove 2 frist case because of nulity
-array_splice($elementsUrl,0,2);
-
-for($i=0; $i < count($elementsUrl); $i++)
-{
-	$elementsUrl[$i] = urldecode($elementsUrl[$i]);
-}
+require('getall.php');
 
 #Execute load and execute the api
 $api = new ApiGenerator();
---
+++ getall.php
@@ -0,0 +1,9 @@
+<?php
+##exploding url for geting info
+//$url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$url = $_SERVER['REQUEST_URI'];
+$elementsUrl = explode("/",$url);
+
+$_SERVER['REQUEST_METHOD']
+
+?>
