06d45dcbc5741b203b07dc664e6a21caefd48742
skad <skadprog@gmail.com>
2014-11-26 07:03:39 +0100

    fussion assigement and using

--- getall.php
+++ getall.php
@@ -2,7 +2,7 @@
 ##exploding url for geting info
 //$url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 $url = $_SERVER['REQUEST_URI'];
-$elementsUrl = explode("/",$url);
+$elementsUrl = explode("/",$_SERVER['REQUEST_URI']);
 
 $_SERVER['REQUEST_METHOD']
 
