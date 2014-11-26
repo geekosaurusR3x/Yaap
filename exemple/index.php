84d0b0cca90677d20873e2b4ca9cf0181c1422c8
skad <skadprog@gmail.com>
2014-11-26 07:04:06 +0100

    move information into api

--- exemple/index.php
+++ exemple/index.php
@@ -2,14 +2,6 @@
 ##declaration
 #return array wich will be print as json
 $return = array();
-#allocate url array;
-$elementsUrl;
-#usr authentification true/false
-$auth = false;
-#group of the usr (permit to limit the access of the api)
-$group = "user";
-#reson of error
-$resson = null;
 
 /** @global int This is this is for sending js or file */
 $return_json = true;
@@ -19,11 +11,10 @@ session_start();
 ##include
 #load include the api class
 require('apigenerator.php');
-require('getall.php');
 
 #Execute load and execute the api
 $api = new ApiGenerator();
-$api->setCache(false);
+$api->getRequest();
 $api->load();
 $return = $api->execute($elementsUrl,$auth,$group);
 
